d8306575afb7f079b2b30afa9bfb2344f0d4a618
Artem Moshnin <tap.kap.tap@gmail.com>
2020-10-05 07:56:57 +0200

    React working

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,21 +3,22 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 // 1) when seeding the database you'll have to uncomment this!
-// import { seedDatabase } from '../seed';
+import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyAaUkF5xgZOwx0NFCfpi1Dw-Iqb6t2H1ok',
+  authDomain: 'panda-v2.firebaseapp.com',
+  databaseURL: 'https://panda-v2.firebaseio.com',
+  projectId: 'panda-v2',
+  storageBucket: 'panda-v2.appspot.com',
+  messagingSenderId: '837964176679',
+  appId: '1:837964176679:web:96dbd9037ac77710b65d02',
+  measurementId: 'G-7QPTXR7VFP',
 };
 
 const firebase = Firebase.initializeApp(config);
 // 2) when seeding the database you'll have to uncomment this!
-// seedDatabase(firebase);
+seedDatabase(firebase);
 // 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
